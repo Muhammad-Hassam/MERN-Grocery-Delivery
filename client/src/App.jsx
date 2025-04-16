64546a7924140984d54269fb3c80e9b59c4bb939
hassamshaikh <hassamshaikh94@gmail.com>
2025-04-16 16:32:42 +0500

    created navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Navbar } from "./components";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
